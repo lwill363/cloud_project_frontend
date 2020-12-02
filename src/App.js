c6d2b8c59b0620b2cf46ca833d6c9fbea8250e6f
Layton Williams <54451152+lwill363@users.noreply.github.com>
2020-12-02 13:46:05 -0500

    Added mapping for values from pass

--- src/App.js
+++ src/App.js
@@ -75,14 +75,15 @@ const Convert = ({ watch, register, required }) => {
 }
 
 const AllConversions = p => {
-
+  let keys = Object.keys(p.pass);
   return (
     <>
       <div className="card" style={{ width: 720 }}>
         <div className="card-body">
           <h5 className="card-title"></h5>
           <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p>{keys[0]}: {p.pass[keys[0]]}</p>
+          <p>{keys[1]}: {p.pass[keys[1]]}</p>
         </div>
       </div>
     </>
